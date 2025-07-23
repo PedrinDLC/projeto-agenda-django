78840315c2c6bb3901046c4edccb95356d6c5153
Pedro Nascimento <pedrohenriqueluiz3@gmail.com>
2025-07-23 12:11:42 -0300

    Criando o model Category e adicionando foreign keey

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 
 # Register your models here.
@@ -24,3 +24,9 @@ class ContactAdmin(admin.ModelAdmin):
         "id",
         "phone",
     )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ordering = ("id",)
