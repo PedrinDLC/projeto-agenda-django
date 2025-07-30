64b4c6f2514e79667b597d43bd2be89a8f43d904
Pedro Nascimento <pedrohenriqueluiz3@gmail.com>
2025-07-30 16:38:08 -0300

    garantindo que o usuario está logado para acessar view

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 from contact.forms import ContactForm
 from contact.models import Contact
 
 
+@login_required(login_url="contact:login")
 def create(request):
     form_action = reverse("contact:create")
 
@@ -30,6 +32,7 @@ def create(request):
     return render(request, "contact/create.html", context)
 
 
+@login_required(login_url="contact:login")
 def update(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
     form_action = reverse("contact:update", args=(contact_id,))
@@ -56,6 +59,7 @@ def update(request, contact_id):
     return render(request, "contact/create.html", context)
 
 
+@login_required(login_url="contact:login")
 def delete(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
